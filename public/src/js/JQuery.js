dc63c77098d64b246f0f303b45487a40be4b3f13
AiceKerime <rafiizzaturohman06@gmail.com>
2022-11-02 00:53:41 +0700

    90% Progress

--- public/src/js/JQuery.js
+++ public/src/js/JQuery.js
@@ -55,27 +55,25 @@ const saveData = () => {
 
     if (editID == null) {
         $.ajax({
-            method: 'POST',
-            url: 'http://localhost:3006/users/add',
-            dataType: 'json',
+            method: "POST",
+            url: "http://localhost:3006/users/add",
+            dataType: "json",
             data: { string, integer, float, date, boolean }
         }).done((data) => {
             readData()
         }).fail((err) => {
-            console.log(err)
             alert('Failed to add data')
         })
     } else {
         $.ajax({
             method: 'PUT',
             url: `http://localhost:3006/users/edit/${editID}`,
-            dataType: 'json',
+            dataType: "json",
             data: { string, integer, float, date, boolean }
         }).done((data) => {
             readData()
         }).fail((err) => {
-            console.log(err)
-            alert('Failed to edit data')
+            alert('Failed to add data')
         })
         editID = null
     }
@@ -89,13 +87,12 @@ const saveData = () => {
 
 // EDIT
 const editData = (user) => {
-    // console.log(user)
     editID = user._id
     $('#string').val(user.string)
     $('#integer').val(user.integer)
     $('#float').val(user.float)
     $('#date').val(moment(user.date).format('YYYY-MM-DD'))
-    $('#boolean').val(JSON.parse(user.boolean))
+    document.getElementById('boolean').value = user.boolean
 }
 
 // DELETE
@@ -160,41 +157,17 @@ function resetData() {
     readData()
 }
 
-function sortTableByColumn(table, column, asc = true) {
-    const dirModifier = asc ? 1 : -1;
-    const tBody = table.tBodies[0];
-    const rows = Array.from(tBody.querySelectorAll("tr"));
-
-    // Sort each row
-    const sortedRows = rows.sort((a, b) => {
-        const aColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
-        const bColText = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
-
-        return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
-    });
-
-    // Remove all existing TRs from the table
-    while (tBody.firstChild) {
-        tBody.removeChild(tBody.firstChild);
+$('th').click(function () {
+    var table = $(this).parents('table').eq(0)
+    var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+    this.asc = !this.asc
+    if (!this.asc) { rows = rows.reverse() }
+    for (var i = 0; i < rows.length; i++) { table.append(rows[i]) }
+})
+function comparer(index) {
+    return function (a, b) {
+        var valA = getCellValue(a, index), valB = getCellValue(b, index)
+        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
     }
-
-    // Re-add the newly sorted rows
-    tBody.append(...sortedRows);
-
-    // Remember how the column is currently sorted
-    table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
-    table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-asc", asc);
-    table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-desc", !asc);
 }
-
-document.querySelectorAll(".table-sortable th").forEach(headerCell => {
-    headerCell.addEventListener("click", () => {
-        const tableElement = headerCell.parentElement.parentElement.parentElement;
-        const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
-        const currentIsAscending = headerCell.classList.contains("th-sort-asc");
-
-        sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
-    });
-});
-
-readData()
+function getCellValue(row, index) { return $(row).children('td').eq(index).text() }
